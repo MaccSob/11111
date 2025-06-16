291e73ee979e98989d77d2c0e917bd8cc527d144
MaccSob <71sobmaciej@gmail.com>
2025-06-16 23:59:53 +0200

    added server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+
+require("dotenv").config();
+const express = require('express');
+const PORT = process.env.PORT;
+const server = express();
+
+
+server.listen(PORT, () => {
+    console.log(`listening on port ${PORT}!`);
+  });
