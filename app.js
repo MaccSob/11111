bb3161d0cfb37185474c38ff6fb58fc17de12bf5
MaccSob <71sobmaciej@gmail.com>
2025-06-20 23:59:29 +0200

    added forms

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/signup',
+    function(req, res, next) {
+      res.render('signup');
+    });
