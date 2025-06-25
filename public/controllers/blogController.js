9c326671f95ba953ebcfaf9eebefd310cab36434
MaccSob <71sobmaciej@gmail.com>
2025-06-25 23:45:36 +0200

    wrong file

--- public/controllers/blogController.js
+++ public/controllers/blogController.js
@@ -1,9 +1,45 @@
-const express = require('express');
-const controller = require('../controllers/blogController');
-const auth = require('../middlewares/auth');
+const blogModel = require('../models/blogs');
 
-const blogRouter = express.Router();
+const createBlog = async (req, res) => {
+  try {
+    const {
+      title,
+      description,
+      tag,
+      author,
+      timestamp,
+      state,
+      read_count,
+      reading_time,
+      body,
+    } = req.body;
+    const user_id = req.user_id;
 
-blogRouter.use(auth.authenticateUser); // Requires authentication for all routes
+   
 
-blogRouter.post('/create', controller.createBlog);
+    const existingBlog = await blogModel.findOne({
+      title: title,
+      description: description,
+      tag: tag,
+      author: author,
+      state: state,
+      user_id: user_id,
+      body: body,
+    });
+
+    
+    const blog = await blogModel.create({
+      title: title,
+      description: description,
+      tag: tag,
+      author: author,
+      state: state,
+      user_id: user_id,
+      body: body,
+    });
+
+    res.status(302).redirect("/dashboard");
+  } catch (error) {
+    console.log(error);
+  }
+};
